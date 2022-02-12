8f36ac4732f6ea7c60b3e8d0d1bb0abfc822af33
Daniel Ferreira <danthuggin@gmail.com>
2022-02-12 16:17:44 -0300

    Implementado mudança de idioma

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const previousMonthButton = document.querySelector('[data-js="previous-month"]');
 const nextMonthButton = document.querySelector('[data-js="next-month"]');
 const calendar = document.querySelector('[data-days=""]');
+const switchLanguage = document.querySelector('#switch-language');
+const week = document.querySelector('[data-js="week-days"]');
 
-const months = {
+/* const months = {
     1: 'January',
     2: 'February',
     3: 'March',
@@ -15,17 +17,63 @@ const months = {
     10: 'Octber',
     11: 'November',
     12: 'December'
+}; */
+
+const getMonths = language => {
+    EN = {
+        1: 'January',
+        2: 'February',
+        3: 'March',
+        4: 'April',
+        5: 'May',
+        6: 'June',
+        7: 'July',
+        8: 'August',
+        9: 'September',
+        10: 'Octber',
+        11: 'November',
+        12: 'December'
+    },
+    PT = {
+        1: 'Janeiro',
+        2: 'Fevereiro',
+        3: 'Março',
+        4: 'Abril',
+        5: 'Maio',
+        6: 'Junho',
+        7: 'Julho',
+        8: 'Agosto',
+        9: 'Setembro',
+        10: 'Outubro',
+        11: 'Novembro',
+        12: 'Dezembro'
+    }
+    return language === 'EN' ? EN : PT;
 };
 
-const weekDays = {
-    1: 'Sun',
-    2: 'Mon',
-    3: 'Tue',
-    4: 'Wed',
-    5: 'Thu',
-    6: 'Fri',
-    7: 'Sat',
-};
+const getWeekDays = language => {
+    const weekDaysEN = {
+        1: 'Sun',
+        2: 'Mon',
+        3: 'Tue',
+        4: 'Wed',
+        5: 'Thu',
+        6: 'Fri',
+        7: 'Sat',
+    };
+
+    const weekDaysPT = {
+        1: 'Dom',
+        2: 'Seg',
+        3: 'Ter',
+        4: 'Qua',
+        5: 'Qui',
+        6: 'Sex',
+        7: 'Sab',
+    };
+
+    return language === 'EN' ? weekDaysEN : weekDaysPT;
+}
 
 const date = new Date();
 const today = date.getDate();
@@ -35,6 +83,7 @@ const year = date.getFullYear();
 
 let currentMonth = month + 1;
 let currentYear = year;
+let language = 'EN';
 
 const getFirstDayOfTheMonth = (year, month) => new Date(year, month).getDay();
 const getLastDayOfTheMonth = (year, month) => new Date(year, month, 0).getDate();
@@ -62,14 +111,19 @@ const getCalendarDays = () => {
     return [...previousDays, ...currentDays, ...nextDays].join('');
 }
 
-const getMonthName = currentMonth => months[currentMonth];
+const getMonthName = currentMonth => getMonths(language)[currentMonth];
 
-const formatCalendar = () => {
-
+const setWeekDays = () => {
+    const weekDays = getWeekDays(language);
+    const weekDaysOnCalendar = Object.values(weekDays).map(value => `<div>${value}</div>`).join('');
+    week.textContent = '';
+    week.insertAdjacentHTML('afterbegin', weekDaysOnCalendar);
 }
 
 const setDateOnCalendar = () => {
     const monthName = getMonthName(currentMonth);
+    const weekDays = getWeekDays(language);
+    const months = getMonths(language);
 
     document.querySelector('[data-js="month-year"]').textContent = `${monthName} ${currentYear}`;
     document.querySelector('[data-js="date"]').textContent =
@@ -83,6 +137,7 @@ const setToday = () => {
     document.querySelector(`[data-day="${today}"]`).classList.add('today');
 }
 
+setWeekDays();
 setDateOnCalendar();
 setToday();
 
@@ -104,5 +159,12 @@ nextMonthButton.addEventListener('click', () => {
         currentYear++
     }
 
+    setDateOnCalendar();
+});
+
+switchLanguage.addEventListener('click', e => {
+    const check = e.target.checked ? 'PT' : 'EN';
+    language = check;
+    setWeekDays();
     setDateOnCalendar();
 });
